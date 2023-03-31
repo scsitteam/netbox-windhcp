70698e0ca6638aa1cac1ba8a82666aae617a8785
Marius Rieder <marius.rieder@scs.ch>
2023-03-31 09:41:14 +0200

    Parse different MAC formats.

--- src/sync/mac.rs
+++ src/sync/mac.rs
@@ -10,8 +10,8 @@ impl<T: std::fmt::UpperHex + num::Num + Default> MacAddr<T> for Vec<T> {
     }
 
     fn from_mac(mac: &str) -> Vec<T> {
-        mac.split(':')
-            .map(|h| T::from_str_radix(h, 16).unwrap_or_default())
+        regex::Regex::new(r"[0-9A-Fa-f]{2}").unwrap().captures_iter(mac)
+            .map(|h| T::from_str_radix(&h[0], 16).unwrap_or_default())
             .collect::<Vec<T>>()
     }
 }
@@ -21,9 +21,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_parses_ips() {
-        let mac = Vec::<u8>::from_mac("00:11");
-        assert_eq!(mac, vec!(0x00, 0x11))
+    fn it_parses_mac() {
+        let mac = Vec::<u8>::from_mac("00:aa:bB:CC");
+        assert_eq!(mac, vec!(0x00, 0xaa, 0xbb, 0xcc))
+    }
+
+    #[test]
+    fn it_parses_mac_w_colon() {
+        let mac = Vec::<u8>::from_mac("00:11:22:33");
+        assert_eq!(mac, vec!(0x00, 0x11, 0x22, 0x33))
+    }
+
+    #[test]
+    fn it_parses_mac_w_dash() {
+        let mac = Vec::<u8>::from_mac("00-11-2233");
+        assert_eq!(mac, vec!(0x00, 0x11, 0x22, 0x33))
+    }
+
+    #[test]
+    fn it_parses_mac_wo_separation() {
+        let mac = Vec::<u8>::from_mac("00112233");
+        assert_eq!(mac, vec!(0x00, 0x11, 0x22, 0x33))
     }
 
     #[test]
